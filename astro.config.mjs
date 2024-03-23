e52b484f49fbcbd0e6a7f6bfc29f8006acf25f02
Mohamed <mosaleh@mail.com>
2024-03-23 16:22:03 +0200

    add lqip plugin with ts definitions for hero images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
+import lqip from "vite-plugin-lqip";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [lqip()],
+  },
   integrations: [
     icon({
       iconDir: "src/icons",
     }),
   ],
 });
-
